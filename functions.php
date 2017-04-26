c47e8feddfe6fc925de026552449218dcc928ff1
fncreative <dann.frey@me.com>
2017-04-25 18:29:19 -0700

    Updates

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+
+Theme Name: Web 170 Flying Lion Brewing Wordpress
+Theme URI: https://github.com/fncreative/web170
+Author: Dann Frey
+Author URI: https://fncreative.design
+Description: This is the theme that will be used for Flying Lion Brewing Webv 170 Project.
+Version: One Point Two
+*/
+
+?>
--- index.php
+++ index.php
@@ -1,24 +1,25 @@
-
-
-<!doctype html>
+<!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="">
-	<title>The Taproom | Flying Lion Brewing</title>
+	<title><?php bloginfo('description'); ?> | <?php bloginfo('name'); ?></title>
 	<link rel="shortcut icon" href="https://flyinglionbrewing.com/img/favicon.png" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css?family=Alegreya+SC:700|Muli" rel="stylesheet">
-		<link rel="stylesheet" href="stylesheet.css" />
-		<link rel="stylesheet" href="main.css" />
+		<link rel="stylesheet" href="stylesheet.css"/>
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>" type="text/css" media="all" />
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script type="text/javascript" src="js/main.js"></script>
+<!-- begin wp head -->
+<?php wp_head(); ?>
+<!-- end wp head -->
 </head>
-<body class="taproom">
+<body <?php body_class(); ?>>
 	<header class="row" id="header">
         <div class="row">
             <div class="column">
-            	                <a href="home.html"><img src="img/flyinglionbrewing2.svg" alt="Flying Lion Brewing" id="logo"></a>
+            	                <a href="home.html"><img src="<?php bloginfo('template_directory'); ?>/img/flyinglionbrewing2.svg" alt="Flying Lion Brewing" id="logo"></a>
                                 <div id="menu" class="hamburger">
 					<span id="line1"></span>
 					<span id="line2"></span>
@@ -43,7 +44,7 @@
 				<p>Whether you’re looking for a taster flight to decide what to fill your favorite hometown growler with, a pint while you choose which of our 4 growler sizes to take home, a liter stein of a delicious German-style brew, or a keg for a housewarming party, we’ve got you covered!</p>
 			</div>
             <div class="col col-2 pad-20">
-            	<img src="https://flyinglionbrewing.com/img/flying-lion-shopfront.jpg" alt="Flying Lion Brewing - Columbia City, Seattle" class="res-image featured-beer" />
+            	<img src="/img/flying-lion-shopfront.jpg" alt="Flying Lion Brewing - Columbia City, Seattle" class="res-image featured-beer" />
             </div>
 		</div>
 	</div>
@@ -107,5 +108,8 @@
 			</div>
 		</footer>
 	</div>
+<!-- begin wp footer -->
+<?php wp_footer(); ?>
+<!-- end wp footer -->
 </body>
 </html>
