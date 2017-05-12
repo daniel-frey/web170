e1b3039144e832666632942f9eb49fb6ffde428b
fncreative <dann.frey@me.com>
2017-05-11 20:52:56 -0700

    see below
    front-page added

--- front-page.php
+++ front-page.php
@@ -34,6 +34,20 @@
 <?php if(have_posts()):while(have_posts()):the_post();?>
 <?php the_content();?>
 <?php endwhile; endif; ?>
+<div class ="row">
+    <div class="column">
+        <div class="col col-1 pad-20">
+        <?php
+            $args = array('numberposts' => '4');
+            $recent_posts = wp_get_recent_posts($args);
+            foreach ($recent_posts as $recent){
+                echo '<div><h2>'.$recent['post_title'].'</h2>'.$recent['post_content'].'</div>';
+            }
+            ?>
+        </div>
+        
+    </div>
+</div>
 </div>
 <div id="spread"></div>
 	<div id="footer" class="content">
